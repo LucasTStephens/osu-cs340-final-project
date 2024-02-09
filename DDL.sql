0f6efe15004dcd20100dc8094bb19566397babd2
LucasTStephens <l.stephens33@me.com>
2024-02-08 20:59:17 -0800

    Finished CustomerAccounts inserts

--- DDL.sql
+++ DDL.sql
@@ -30,14 +30,48 @@ CREATE OR REPLACE TABLE Employees (
     PRIMARY KEY (employeeID)
 );
 
-CREATE OR REPLACE TABLE Customers (
-    customerID int(11) AUTO_INCREMENT,
+CREATE OR REPLACE TABLE CustomerSales (
+    saleID int(11) AUTO_INCREMENT,
     systemID int(11) NOT NULL,
     customerEmail varchar(255) NOT NULL,
     timeIn datetime NOT NULL,
-    timeOut datetime NOT NULL,
+    timeOut datetime,
     PRIMARY KEY (customerID),
     FOREIGN KEY (systemID) REFERENCES GameSystems(systemID),
     FOREIGN KEY (customerEmail) REFERENCES CustomerAccounts(customerEmail)
 );
 
+INSERT INTO CustomerAccounts (customerEmail, first_name, last_name, customerDOB)
+VALUES ('jonbovi@aol.com', 'Jon', 'Bovi', '1989-01-01'),
+('cloud99@aol.com', 'Darcy', 'Wiggins', '1985-12-05'),
+('dmenace@aol.com', 'Katy', 'Combs', '1996-03-28'),
+('rssbob@aol.com', 'Shaun', 'Mitchell', '1976-04-04'),
+('poohwin@aol.com', 'Nettie', 'Shannon', '2000-02-28');
+
+INSERT INTO CustomerSales (saleID, systemID,)
+VALUES (),
+(),
+(),
+(),
+();
+
+INSERT INTO Employees ()
+VALUES (),
+(),
+(),
+(),
+();
+
+INSERT INTO GameSystems ()
+VALUES (),
+(),
+(),
+(),
+();
+
+INSERT INTO Lounges ()
+VALUES (),
+(),
+(),
+(),
+();
